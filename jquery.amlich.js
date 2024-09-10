2936f37f8a61a1399c7adf1d6f8b14305903f0ef
phuochq <33310761+phuochq@users.noreply.github.com>
2024-09-10 15:57:28 +0700

    Update jquery.amlich.js

--- jquery.amlich.js
+++ jquery.amlich.js
@@ -521,9 +521,6 @@
       }
       res += '</tr>\n';
     }
-    res += '<tr>\n';
-    res += '  <td class="tenthang">'+monthName+'</td>\n';
-    res += '</tr>\n';
     res += '</tbody>\n';
     res += '</table>\n';
     return res;
